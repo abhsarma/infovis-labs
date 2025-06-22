a56f9442a8170f7a7efbb160702e8cc2c8f661d6
Abhraneel Sarma <abhraneelsarma2024@u.northwestern.edu>
2025-06-22 15:33:37 -0500

    project structure

--- lab1/src/routes/+page.svelte
+++ lab1/src/routes/+page.svelte
@@ -1,22 +1,10 @@
 <script>
-/* Javascript goes here */
-	let margin = { top: 20, right: 20, bottom: 20, left: 20 };
-	let width = 400 - margin.left - margin.right;
-	let height = 400 - margin.top - margin.bottom;
-
-	let dataset = [
-		[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-		[410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-	];
+	/* Javascript goes here */
+	import Lab1 from "./components/lab1.svelte"
 </script>
 
 <main>
-	<div>
-		<h1>Lab 1</h1>
-		<svg height="{height}px" width="{width}px">
-			
-		</svg>
-	</div>
+	<Lab1/>
 </main>
 
 <style>
---
+++ lab1/src/routes/components/lab1.svelte
@@ -0,0 +1,17 @@
+<script>
+/* Javascript goes here */
+	let margin = { top: 20, right: 20, bottom: 20, left: 20 };
+	let width = 400 - margin.left - margin.right;
+	let height = 400 - margin.top - margin.bottom;
+</script>
+
+<div>
+	<h1>Lab 1</h1>
+	<svg height="{height}px" width="{width}px">
+		
+	</svg>
+</div>
+
+<style>
+/*  CSS styles go here  */
+</style>
