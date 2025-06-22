46c7cbc58b526c0a68cd1b986defb8c79ceb397f
Abhraneel Sarma <abhraneelsarma2024@u.northwestern.edu>
2025-06-22 15:12:15 -0500

    lab 1 contents

--- lab1/src/routes/+page.svelte
+++ lab1/src/routes/+page.svelte
@@ -3,10 +3,16 @@
 	let margin = { top: 20, right: 20, bottom: 20, left: 20 };
 	let width = 400 - margin.left - margin.right;
 	let height = 400 - margin.top - margin.bottom;
+
+	let dataset = [
+		[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+		[410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+	];
 </script>
 
 <main>
 	<div>
+		<h1>Lab 1</h1>
 		<svg height="{height}px" width="{width}px">
 			
 		</svg>
