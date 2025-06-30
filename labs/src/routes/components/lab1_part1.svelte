26b31e22ca18b428bbfa487669b56feb0efbe727
Abhraneel Sarma <abhraneelsarma2024@u.northwestern.edu>
2025-06-30 14:49:32 -0500

    minor fix

--- labs/src/routes/components/lab1_part1.svelte
+++ labs/src/routes/components/lab1_part1.svelte
@@ -1,9 +1,12 @@
 <script>
+	import {scaleLinear} from "d3-scale";
+	import {max} from "d3-array";
+
 /* Javascript goes here */
 	let margin = { top: 20, right: 20, bottom: 20, left: 20 };
 	let width = 400 - margin.left - margin.right;
 	let height = 400 - margin.top - margin.bottom;
-
+	
 	// Add your answers to Part 1 Question 5 here
 	// scale(300) -> 180
 	// scale(100) ->
@@ -20,13 +23,16 @@
 <div>
 	<h1>Lab 1</h1>
 	<svg 
-		height="{height}px" 
-		width="{width}px"
+		height="400px" 
+		width="400px"
 		style="background-color: #eee;"
 		>
 		<!-- you can remove the style line above -->
 		<g>
-
+			{#each dataset as d}
+				<!-- {console.log(d)} -->
+				<circle cx="{xScale(d[0])}" cy="{yScale(d[1])}" r="5"/>
+			{/each}
 		</g>
 	</svg>
 </div>
